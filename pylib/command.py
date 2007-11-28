cbff95cb6b205594d59ed907af1a8832c88145ca
Alon Swartz <alon@sterilesecurity.com>
2007-11-28 16:29:00 +0200

    imported command.py, popen4.py, fifobuffer.py and fileevent.py from connagent

---
+++ pylib/command.py
@@ -0,0 +1,383 @@
+"A convenient interface for controlling commands"
+
+import os
+import signal
+import time
+import select
+import re
+import sys
+import popen4
+from fifobuffer import FIFOBuffer
+from fileevent import *
+
+def mkarg(x):
+    """escape an argument from shell meta characters"""
+    if '\'' not in x:
+        return ' \'' + x + '\''
+    s = ' "'
+    for c in x:
+        if c in '\\$"`':
+            s = s + '\\'
+        s = s + c
+    s = s + '"'
+    return s
+
+def pid_free(pid):    
+    try:
+        retval = os.waitpid(pid, os.WNOHANG)
+        if retval[0] == pid:
+            return retval[1]
+    except:
+        return
+    try:
+        os.kill(pid, signal.SIGKILL)
+        retval = os.waitpid(pid, 0)
+        if retval[0] == pid:
+            return retval[1]
+    except:
+        return None
+
+    
+def set_blocking(fd, block):
+    import fcntl
+    arg = os.O_NONBLOCK
+    if block:
+        arg =~ arg
+    fcntl.fcntl(fd, fcntl.F_SETFL, arg)
+
+
+class Command:
+    """Convenience module for executing a command
+    Usage:
+
+    c = Command("./test.py")
+    if c.state() == c.STATE_RUNNING:
+        c.wait()
+
+    print c.output()
+
+    c = Command("./test.py")
+    while c.output() is None:
+        time.sleep(1)
+
+    print "output = '%s', exitcode = %d" % (c.output(), c.exitcode())
+    
+    """
+    STATE_RUNNING = 0
+    STATE_FINISHED = 1
+    STATE_TERMINATED = -1
+
+    class Error(Exception):
+        pass
+
+    class _ChildObserver(Observer):
+        def __init__(self, command):
+            self.command = command
+
+        def notify(self, subject, event, val):
+
+            if event in ('read', 'readline'):
+                self.command._dlog("# EVENT '%s':\n%s" % (event, val))
+                self.command._output.write(val)
+            elif event in ('readlines', 'xreadlines'):
+                self.command._dlog("# EVENT '%s':\n%s" % (event, "".join(val)))
+                self.command._output.write("".join(val))
+
+    def __init__(self, cmd, runas=None, pty=False, setpgrp=False, debug=False, observeOutput=True):
+        """Args:
+        'cmd' what command to execute
+            Can be a list ("/bin/ls", "-la")
+            or a string "/bin/ls" (will be passed to sh -c)
+
+        'pty' do we allocate a pty for command?
+        'runas' user we run as (set user, set groups, etc.)
+        'setpgrp' do we setpgrp in child? (create its own process group)
+        """
+        
+        self._child = popen4.Popen4(cmd, 0, pty, runas, setpgrp)
+        self.pid = self._child.pid
+
+        self._setpgrp = setpgrp
+        self._debug = debug
+        self._cmd = cmd
+        self._state = Command.STATE_RUNNING
+        self._exitcode = None
+        
+        self._output = FIFOBuffer()
+
+        if observeOutput:
+            self._child.fromchild = FileEventAdaptor(self._child.fromchild)
+            self._child.fromchild.addObserver(self._ChildObserver(self))
+        
+        self._dlog("# command started (pid=%d, pty=%s): %s" % (self._child.pid,
+                                                               `pty`,
+                                                               cmd))
+
+    def __del__(self):
+        pid_free(self._child.pid)
+        
+    def _dlog(self, msg):
+        if self._debug:
+            print >> sys.stderr, msg
+        
+    def terminate(self, gracetime=0, sig=signal.SIGTERM):
+        """terminate command. kills command with 'sig', then sleeps for 'gracetime', before sending SIGKILL
+        """
+
+        if self.status() == Command.STATE_RUNNING:
+            pid = self.pid
+            if self._setpgrp:
+                pid = -pid
+
+            os.kill(pid, sig)
+            time.sleep(gracetime)
+            if self.status() != Command.STATE_FINISHED:
+                pid_free(pid)
+                self._dlog("# command (pid %d) terminated" % self._child.pid)
+                self._state = Command.STATE_TERMINATED
+
+    def status(self):
+        """return the status of the command:
+        STATE_RUNNING		Command still running
+        STATE_FINISHED		Command finished (or handled termination gracefully)
+        STATE_TERMINATED	Command terminated
+        """
+        if self._state != Command.STATE_RUNNING:
+            return self._state
+
+        if self._child.poll() == -1:
+            return Command.STATE_RUNNING
+
+        self._dlog("# command (pid %d) finished" % self._child.pid)
+        self._state = Command.STATE_FINISHED
+        return self._state
+
+    def exitcode(self):
+        """return the command's exitcode"""
+        if self._exitcode is not None:
+            return self._exitcode
+
+        if self.status() != Command.STATE_FINISHED:
+            return None
+
+        self._exitcode = self._child.poll() >> 8
+        return self._exitcode
+
+    def wait(self, timeout=0, interval=0.2):
+        """wait for process to finish executing.
+        'timeout' is how long we wait (in seconds)
+        'interval' is how long we sleep between checks to see if process has finished
+        return value: did the process finish? True/False
+
+        """
+        if self.status() == Command.STATE_FINISHED:
+            return True
+        
+        if timeout == 0:
+            self._child.wait()
+            return True
+        else:
+            start=time.time()
+            while time.time() - start < timeout:
+                if self.status() == Command.STATE_FINISHED:
+                    return True
+                time.sleep(interval)
+            return False
+
+    def output(self):
+        """return the command's output as a string
+
+        LIMITATIONS: unless we are using outputsearch, output will be empty until the command finished.
+        """
+        if len(self._output):
+            return self._output.getvalue()
+        
+        if self.status() != Command.STATE_FINISHED:
+            return None
+
+        self._child.fromchild.read()
+        return self._output.getvalue()
+
+    def outputfh(self):
+        """return the command's filehandler.
+
+        NOTE: this file handler magically updates self._output"""
+        return self._child.fromchild
+        
+    def outputsearch(self, p, timeout=0, linemode=False):
+        """Search for 'p' in the command's output, while listening for more output from command, within 'timeout'
+
+        'p' can be a list of re patterns or a single re pattern
+           the value of a pattern can be an re string, or a compiled re object
+        If 'timeout' is 0, wait forever [*]
+
+	'linemode' determines whether we search output line by line (as it comes), or all of the output in aggregate
+        
+        Return value:
+        Did we match the output?
+            Return a tuple (the pattern we matched, the string match)
+        Otherwise (timeout/HUP) Return empty tuple ()
+
+        Side effects:
+        - If we HUP, we wait for the process to finish.
+          You can check the status() to see if the process is still running.
+        - Output is collected and can be accessed by the output() method [*]
+        """
+        patterns = []
+        if not type(p) in (tuple, list):
+            patterns.append(p)
+        else:
+            patterns += p
+
+        # compile all patterns into re objects, but keep the original pattern object
+        # so we can return it to the user when we match (friendlier interface)
+        re_type = type(re.compile(""))
+        for i in xrange(len(patterns)):
+            if type(patterns[i]) is not re_type:
+                patterns[i] = (re.compile(patterns[i]), patterns[i])
+            else:
+                patterns[i] = (patterns[i], patterns[i])
+
+        def check_match():
+            if linemode:
+                while 1:
+                    line = self._output.readline(True)
+                    if not line:
+                        return None
+                    
+                    for pattern_re, pattern_orig in patterns:
+                        match = pattern_re.search(line)
+                        if match:
+                            return pattern_orig, match
+
+                    if not line.endswith('\n'):
+                        return None
+                        
+            else:
+                # match against the entire buffered output
+                for pattern_re, pattern_orig in patterns:
+                    match = pattern_re.search(self._output.getvalue())
+                    if match:
+                        return pattern_orig, match
+            
+        # maybe we already match? (in buffered output)
+        m = check_match()
+        if m:
+            return m
+        
+        fh = self.outputfh()
+
+        def handle_events(poll_events):
+            fd, mask = poll_events[0]
+            if mask & select.POLLIN:
+                fh.read()
+                match = check_match()
+                if match:
+                    return match
+
+            if mask & select.POLLHUP:
+                self.wait()
+                return ()
+
+        def poll_for_new_output():
+            p = select.poll()
+            p.register(fh.fileno(), select.POLLIN | select.POLLHUP)
+            
+            started = time.time()
+            if timeout:
+                while time.time() - started < timeout:
+                    time_elapsed = time.time() - started
+                    try:
+                        events = p.poll(timeout - time_elapsed)
+                    except select.error:
+                        continue
+                    if events:
+                        ret = handle_events(events)
+                        if ret is not None:
+                            return ret
+            else:
+                while 1:
+                    try:
+                        events = p.poll()
+                    except select.error:
+                        continue
+                    if events:
+                        ret = handle_events(events)
+                        if ret is not None:
+                            return ret
+
+            return None
+
+        set_blocking(fh.fileno(), 0)
+        ret = poll_for_new_output()
+        try:
+            set_blocking(fh.fileno(), 1)
+        except:
+            pass
+
+        return ret
+        
+class CommandTrue:
+    """
+    Simplified interface to Command class.
+    
+    A command istrue() if its exitcode == 0
+    """
+    def __init__(self, cmd):
+        self._c = Command(cmd, observeOutput=False)
+        self._istrue = None
+
+    def wait(self, timeout=0):
+        return self._c.wait(timeout)
+
+    def terminate(self):
+        self._c.terminate()
+
+    def istrue(self):
+        if self._istrue is not None:
+            return self._istrue
+        
+        exitcode = self._c.exitcode()
+        if exitcode is None:
+            return None
+
+        if exitcode:
+            self._istrue = False
+        else:
+            self._istrue = True
+
+        return self._istrue
+
+last_exitcode = None
+last_output = None
+
+def eval(cmd, setpgrp=False):
+    """convenience function
+    execute 'cmd' and return True/False is exitcode == 0
+
+    Side effect: sets command.last_exitcode and command.last_output
+    """
+    global last_output
+    global last_exitcode
+    
+    c = Command(cmd, setpgrp=setpgrp)
+    c.wait()
+    last_output = c.output()
+    last_exitcode = c.exitcode()
+    return c.exitcode() == 0
+
+def output(cmd):
+    """convenience function
+    execute 'cmd' and return it's output
+
+    Side effect: sets command.last_exitcode and command.last_output
+
+    """
+    global last_output
+    global last_exitcode
+
+    c = Command(cmd)
+    c.wait()
+    last_output = c.output()
+    last_exitcode = c.exitcode()
+    return c.output()
---
+++ pylib/popen4.py
@@ -0,0 +1,174 @@
+import sys
+import os
+import termios
+import pty
+import signal
+import pwd
+import grp
+
+_active = []
+
+try:
+    MAXFD = os.sysconf('SC_OPEN_MAX')
+except (AttributeError, ValueError):
+    MAXFD = 256
+
+def _cleanup():
+    for inst in _active[:]:
+        inst.poll()
+
+class CatchIOErrorWrapper:
+    """wraps around a file handler and catches IOError exceptions"""
+
+    def __init__(self, fh):
+        self.fh  = fh
+
+    def __del__(self):
+        try:
+            self.fh.close()
+        except IOError:
+            pass
+
+    def __getattr__(self, attr):
+        return getattr(self.fh, attr)
+        
+    def read(self, size=-1):
+        try:
+            return self.fh.read(size)
+        except IOError:
+            return ''
+            
+    def readline(self, size=-1):
+        try:
+            return self.fh.readline(size)
+        except IOError:
+            return ''
+
+    def readlines(self, size=-1):
+        try:
+            return self.fh.readlines(size)
+        except IOError:
+            return []
+
+    def xreadlines(self):
+        return self.fh.xreadlines()
+
+class Popen4:
+    """An implementation of popen2.Popen4 that can allocates a pty or a pipe for
+    the executed command.
+
+    Ptys are useful for cases where the unix buffering is going to ruin your day.
+    
+    """
+
+    sts = -1
+
+    def __init__(self, cmd, bufsize=0, pty=False, runas=None, setpgrp=False):
+        """'runas' has to be a username"""
+        self.childerr = None
+        if pty:
+            self._init_pty(cmd, bufsize, runas, setpgrp)
+        else:
+            self._init_pipe(cmd, bufsize, runas, setpgrp)
+#        _active.append(self) # DEBUG TEST
+
+    def _init_pty(self, cmd, bufsize, runas, setpgrp):
+        def tty_echo_off(fd):
+            new = termios.tcgetattr(fd)
+            new[3] = new[3] & ~termios.ECHO          # lflags
+            termios.tcsetattr(fd, termios.TCSANOW, new)
+
+        (pid, fd) = pty.fork()
+        if not pid:
+            # Child
+            if setpgrp:
+                os.setpgrp()
+            if runas is not None:
+                self._drop_privileges(runas)
+            self._run_child(cmd)
+
+        tty_echo_off(fd)
+        
+        self.pid = pid
+        self.fromchild = CatchIOErrorWrapper(os.fdopen(fd, "r+", bufsize))
+        self.tochild = self.fromchild
+
+    def _init_pipe(self, cmd, bufsize, runas, setpgrp):
+        p2cread, p2cwrite = os.pipe()
+        c2pread, c2pwrite = os.pipe()
+        self.pid = os.fork()
+        if self.pid == 0:
+            # Child
+            if setpgrp:
+                os.setpgrp()
+            if runas is not None:
+                self._drop_privileges(runas)
+            os.dup2(p2cread, 0)
+            os.dup2(c2pwrite, 1)
+            os.dup2(c2pwrite, 2)
+            
+            self._run_child(cmd)
+        os.close(p2cread)
+        self.tochild = os.fdopen(p2cwrite, 'w', bufsize)
+        os.close(c2pwrite)
+        self.fromchild = os.fdopen(c2pread, 'r', bufsize)
+
+    def _run_child(self, cmd):
+        if isinstance(cmd, basestring):
+            cmd = ['/bin/sh', '-c', cmd]
+        for i in range(3, MAXFD):
+            try:
+                os.close(i)
+            except OSError:
+                pass
+        try:
+            os.execvp(cmd[0], cmd)
+        finally:
+            os._exit(1)
+    
+    def __del__(self):
+        self.poll()
+        try:
+            self.fromchild.close()
+            self.tochild.close()
+        except:
+            pass
+            
+    def _drop_privileges(self, user):
+        pwent = pwd.getpwnam(user)
+        uid, gid, home = pwent[2], pwent[3], pwent[5]
+        os.unsetenv("XAUTHORITY")
+        os.putenv("USER", user)
+        os.putenv("HOME", home)
+
+        usergroups = []
+        groups = grp.getgrall()
+        for group in groups:
+            if user in group[3]:
+                usergroups.append(group[2])
+        
+        os.setgroups(usergroups)
+        os.setgid(gid)
+        os.setuid(uid)
+
+    def poll(self):
+        """Return the exit status of the child process if it has finished,
+        or -1 if it hasn't finished yet."""
+        if self.sts < 0:
+            try:
+                pid, sts = os.waitpid(self.pid, os.WNOHANG)
+                if pid == self.pid:
+                    self.sts = sts
+#                    _active.remove(self) # DEBUG TEST
+            except os.error:
+                pass
+        return self.sts
+
+    def wait(self):
+        """Wait for and return the exit status of the child process."""
+        pid, sts = os.waitpid(self.pid, 0)
+        if pid == self.pid:
+            self.sts = sts
+#            _active.remove(self) # DEBUG TEST
+        return self.sts
+    
