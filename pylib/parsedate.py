eeca963b34f84699b418ba1a22edc72275c91ed0
Liraz Siri <liraz@sterilesecurity.com>
2007-12-05 13:38:12 +0200

    turned parsedate into a universal date parser
    rational: if parsedate supports many formats, the user doesn't have to know
    which specific format to use.
    
    supported date formats:
       N days|months|weeks|years ago
    
       DD/MM[/YY[YY]]
       DD-MM[-YY[YY]]
       DD.MM[.YY[YY]]
    
       YYYY/MM[/DD]
       YYYY-MM[-DD]
       YYYY.MM[.DD]
    
       YYYY

--- pylib/parsedate.py
+++ pylib/parsedate.py
@@ -1,36 +1,83 @@
+import re
 import datetime
 
 class DateError(Exception):
     pass
 
-class Date(datetime.date):
-    def __str__(self):
-        return "%04d/%02d/%02d" % (self.year, self.month, self.day)
-        
 def parsedate(datestr):
-    """Parse <datestr> > datetime.date() instance
-    <datestr> := YY[YY][/MM[/DD]]
-    """
-    vals = datestr.split("/")
-    try:
-        if len(vals) == 3:
-            year, month, day = map(int, vals)
-        elif len(vals) == 2:
-            year, month = map(int, vals)
-            day = 1
-        elif len(vals) == 1:
-            year = int(vals[0])
-            month = 1
-            day = 1
-        else:
-            raise ValueError()
-    except ValueError:
-        raise DateError("illegal date (%s)" % datestr)
+    """Universal date parser -> return a datetime.date() instance
+    Supported date formats:
+       N days|months|weeks|years ago
 
-    if year < 100:
-        if year > 70:
-            year += 1900
-        else:
-            year += 2000
+       DD/MM[/YY[YY]]
+       DD-MM[-YY[YY]]
+       DD.MM[.YY[YY]]
+
+       YYYY/MM[/DD]
+       YYYY-MM[-DD]
+       YYYY.MM[.DD]
+
+       YYYY
+    """
+    try:
+        year = int(datestr)
+        if year >= 1000:
+            return datetime.date(year, 1, 1)
+    except ValueError:
+        pass
+
+    m = re.match(r'(\d+)\s+(day|month|week|year)s?\s*(?:ago)?$', datestr)
+    if m:
+        num = int(m.group(1))
+        unit = m.group(2)
+
+        if unit == 'week':
+            num *= 7
+        elif unit == 'month':
+            num *= 30
+        elif unit == 'year':
+            num *= 365
+
+        days = num
+        return datetime.date.today() - datetime.timedelta(days)
+
+    m = re.match(r'(\d\d?)/(\d\d?)(?:/(\d\d\d?\d?))?$', datestr)
+    if not m:
+        m = re.match(r'(\d\d?)-(\d\d?)(?:-(\d\d\d?\d?))?$', datestr)
+        if not m:
+            m = re.match(r'(\d\d?)\.(\d\d?)(?:\.(\d\d\d?\d?))?$', datestr)
         
-    return Date(year, month, day)
+    if m:
+        day = int(m.group(1))
+        month = int(m.group(2))
+        year = int(m.group(3) or datetime.date.today().year)
+        if year < 100:
+            if year > 70:
+                year += 1900
+            else:
+                year += 2000
+
+        return datetime.date(year, month, day)
+    
+
+    m = re.match(r'(\d\d\d\d)/(\d\d?)(?:/(\d\d?))?$', datestr)
+    if not m:
+        m = re.match(r'(\d\d\d\d)-(\d\d?)(?:-(\d\d?))?$', datestr)
+        if not m:
+            m = re.match(r'(\d\d\d\d)\.(\d\d?)(?:\.(\d\d?))?$', datestr)
+            
+    if m:
+        year = int(m.group(1))
+        month = int(m.group(2))
+        if m.group(3):
+            day = int(m.group(3))
+        else:
+            day = 1
+            
+        return datetime.date(year, month, day)
+
+    raise DateError("illegal date (%s)" % datestr)
+
+
+        
+
