79ed9d32fdcbcbc6dfa4cba958a539091ceb7139
Liraz Siri <liraz@sterilesecurity.com>
2007-12-11 07:17:32 +0200

    added paged.py module

---
+++ pylib/paged.py
@@ -0,0 +1,49 @@
+"""This modules provides an stdout instances which
+redirect output through a pager if:
+  A) PAGER is configured in the environment
+  B) stdout is not a tty
+"""
+
+import os
+import sys
+import errno
+
+class _PagedStdout:
+    # lazy definition of pager attribute so that we
+    # execute pager the first time we need it
+    def pager(self):
+        if hasattr(self, '_pager'):
+            return self._pager
+
+        pager = None
+        if os.isatty(sys.stdout.fileno()):
+            pager_env = os.environ.get('PAGER')
+            if pager_env:
+                pager = os.popen(pager_env, "w")
+
+        self._pager = pager
+        return pager
+    pager = property(pager)
+
+    def write(self, text):
+        if self.pager:
+            try:
+                self.pager.write(text)
+                self.pager.flush()
+                
+            except IOError, e:
+                if e[0] != errno.EPIPE:
+                    raise
+        else:
+            sys.stdout.write(text)
+            sys.stdout.flush()
+
+stdout = _PagedStdout()
+
+def test():
+    print >> stdout, "foo"
+    stdout.write("bar")
+
+if __name__=="__main__":
+    test()
+
