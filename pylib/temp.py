425098dee006b2877f5f4de5118ccaf973d607e1
Liraz Siri <liraz@sterilesecurity.com>
2008-02-14 05:04:29 +0200

    temp.py: added simple TempDir class (self deleting temporary directory)

--- pylib/temp.py
+++ pylib/temp.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+import shutil
 
 class TempFile(file):
     def __init__(self, prefix='tmp', suffix=''):
@@ -10,3 +11,18 @@ class TempFile(file):
 
     def __del__(self):
         os.remove(self.path)
+
+class TempDir:
+    def __init__(self, prefix='tmp', suffix='', dir=None):
+        self.path = tempfile.mkdtemp(suffix, prefix, dir)
+
+    def remove(self):
+        shutil.rmtree(self.path)
+        
+    def __del__(self):
+        self.remove()
+
+        
+        
+        
+
