37f471c256fa9e13d50e162a6e29d6a32ade322c
Liraz Siri <liraz@sterilesecurity.com>
2010-03-28 23:41:38 +0300

    improved interface of TempDir by making it inherit from built-in string

--- pylib/temp.py
+++ pylib/temp.py
@@ -20,15 +20,19 @@ class TempFile(file):
         if self.pid == os.getpid():
             os.remove(self.path)
 
-class TempDir:
-    def __init__(self, prefix='tmp', suffix='', dir=None):
-        self.path = tempfile.mkdtemp(suffix, prefix, dir)
+class TempDir(str):
+    def __new__(cls, prefix='tmp', suffix='', dir=None):
+        path = tempfile.mkdtemp(suffix, prefix, dir)
+        self = str.__new__(cls, path)
+
         self.pid = os.getpid()
+        self.path = path
+        return self
 
     def remove(self):
-        shutil.rmtree(self.path)
-        
+        if exists(self):
+            shutil.rmtree(self)
+
     def __del__(self):
-        # sanity check in case we're creating sub-processes (e.g,. stdtrap)
         if self.pid == os.getpid():
             self.remove()
