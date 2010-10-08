195f9a70153bc18ca41f639443176d324b175da4
Liraz Siri <liraz@sterilesecurity.com>
2010-10-08 05:03:19 +0200

    added pidlock module

---
+++ pylib/pidlock.py
@@ -0,0 +1,84 @@
+import os
+from os.path import *
+import fcntl
+import errno
+
+class Locked(Exception):
+    pass
+    
+def pid_exists(pid):
+    try:
+        os.kill(pid, 0)
+    except OSError, e:
+        if e.errno == errno.ESRCH:
+            return False
+
+    return True
+
+class PidLock:
+    def __init__(self, filename, nonblock=False):
+        self.filename = filename
+        self.nonblock = nonblock
+        self.locked = False
+        self.fh = None
+
+    def lock(self, nonblock=None):
+        if exists(self.filename):
+            try:
+                pid = int(file(self.filename).read())
+                if not pid_exists(pid):
+                    os.remove(self.filename)
+            except ValueError:
+                pass
+
+        flags = 0
+        if nonblock is None:
+            nonblock = self.nonblock 
+        if nonblock:
+            flags = fcntl.LOCK_NB
+
+        self.fh = file(self.filename, "w+")
+
+        try:
+            fcntl.flock(self.fh.fileno(), fcntl.LOCK_EX | flags)
+            print >> self.fh, "%d" % os.getpid()
+            self.fh.flush()
+        except IOError, e:
+            if e.errno == errno.EWOULDBLOCK:
+                raise Locked()
+
+        self.locked = True
+
+    def unlock(self):
+        if not self.locked:
+            return
+
+        fcntl.flock(self.fh.fileno(), fcntl.LOCK_UN)
+        self.fh = None
+
+        self.locked = False
+
+    def __del__(self):
+        self.unlock()
+
+# run this twice for best effect
+def _test():
+    import time
+    
+    def sleep(n):
+        print "sleeping for %d seconds" % n
+        time.sleep(n)
+
+    l = PidLock("lock.lock", nonblock=False)
+    l.lock()
+    sleep(5)
+    l.unlock()
+
+    sleep(0.1)
+
+    l.lock()
+    sleep(5)
+    l.unlock()
+
+if __name__ == '__main__':
+    _test()
