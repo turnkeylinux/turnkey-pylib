370bd82297e6c5bbcd1fb027df5c43b7dbbaea8e
Liraz Siri <liraz@turnkeylinux.org>
2011-07-13 16:46:04 +0300

    added copyright header to multiprocessing_utils and threadloop

--- pylib/multiprocessing_utils.py
+++ pylib/multiprocessing_utils.py
@@ -1,3 +1,12 @@
+# Copyright (c) 2011 Liraz Siri <liraz@turnkeylinux.org>
+# 
+# This file is part of turnkey-pylib.
+# 
+# turnkey-pylib is open source software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+
 import signal
 
 import time
